b02524b99ccc9bb66ef251e3ca48ee8bab6849cf
Robin Hundt <24554122+robinhundt@users.noreply.github.com>
2023-02-27 21:49:42 +0100

    Update bench scripts

--- bench_aes_cbc.sh
+++ bench_aes_cbc.sh
@@ -2,9 +2,12 @@
 
 id=$1
 
+dir=$(pwd)
+cd build-release
 make aescbc -j 30
+cd $dir
 
-for in_blocks in {1,2,8,16,32,};do
+for in_blocks in {1,2,8,16,32,64,128,256,512,1024};do
   if [[ $id == 0 ]];then
     heaptrack="$HOME/heaptrack/build/bin/heaptrack -o heaptrack_MOTION_$in_blocks"
   fi
