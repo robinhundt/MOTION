155ad32066eeaef48b5d5c92c290896814b1d066
Robin Hundt <24554122+robinhundt@users.noreply.github.com>
2023-02-27 18:58:36 +0100

    Update bench scripts

--- bench_aes_cbc.sh
+++ bench_aes_cbc.sh
@@ -7,5 +7,5 @@ for in_blocks in {1,2,8,16,32,64};do
   if [[ $id == 0 ]];then
     heaptrack="$HOME/heaptrack/build/bin/heaptrack -o heaptrack_MOTION_$in_blocks"
   fi
-   $heaptrack ./build-release/bin/aescbc --my-id $id --parties 0,130.83.125.166,7745 1,130.83.125.167,7744 --data-bytes $(($in_blocks * 128))
+   $heaptrack ./build-release/bin/aescbc --my-id $id --parties 0,130.83.125.166,7745 1,130.83.125.167,7744 --data-bytes $(($in_blocks * 128)) > MOTION_block_$in_blocks.log
 done
